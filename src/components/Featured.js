652cf2baad7173defd6f77a40d0eeb6062792c35
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-03-16 16:42:56 +0100

    Update Featured.js
    map method

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-// import BTC from '../assets/btc-img.png'
-import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
 import './Featured.css'
+import CoinCard from './CoinCard'
 
 const Featured = () => {
 
@@ -36,145 +35,10 @@ const Featured = () => {
 
                 {/* Right */}
                 <div className='right'>
-                    <div className='card'>
-                        <div className='top'>
-                            {/* <img src={BTC} alt='/' /> */}
-                            <img src={data[0].image} alt='' />
-                        </div>
-
-                        <div>
-                            <h5>{data[0].name}</h5>
-                            <p>${data[0].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[0].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[0].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[0].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div>  {/* className='card' */}
-
-                    <div className='card'>
-                        <div className='top'>
-                            <img src={data[1].image} alt='' />
-                        </div>
-                        
-                        <div>
-                            <h5>{data[1].name}</h5>
-                            <p>${data[1].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[1].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[1].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[1].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div>  {/* className='card' */}
-
-                    <div className='card'>
-                        <div className='top'>
-                            <img src={data[2].image} alt='' />
-                        </div>
-
-                        <div>
-                            <h5>{data[2].name}</h5>
-                            <p>${data[2].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[2].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[2].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[2].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div>  {/* className='card' */}
-
-                    <div className='card'>
-                        <div className='top'>
-                            <img src={data[3].image} alt='' />
-                        </div>
-
-                        <div>
-                            <h5>{data[3].name}</h5>
-                            <p>${data[3].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[3].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[3].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[3].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div>  {/* className='card' */}
-
-                    <div className='card'>
-                        <div className='top'>
-                            <img src={data[4].image} alt='' />
-                        </div>
-
-                        <div>
-                            <h5>{data[4].name}</h5>
-                            <p>${data[4].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[0].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[4].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[4].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div> {/* className='card' */}
-
-                    <div className='card'>
-                        <div className='top'>
-                            <img src={data[5].image} alt='' />
-                        </div>
-
-                        <div>
-                            <h5>{data[5].name}</h5>
-                            <p>${data[5].current_price.toLocaleString()}</p>
-                        </div>
-
-                        {data[5].price_change_percentage_24h < 0 ? (
-                            <span className='red'>
-                                <FiArrowDown className='icon' />
-                                {data[5].price_change_percentage_24h.toFixed(2)}%
-                            </span>
-                        ) : (
-                                <span className='green'>
-                                    <FiArrowUpRight className='icon' />
-                                    {data[5].price_change_percentage_24h.toFixed(2)}%
-                                </span>
-                            )}
-                    </div>   {/* className='card' */}
-
+                    {data.map(data => (
+                        <CoinCard key={data.id}
+                        data={data}/>
+                    ))}
                 </div>   {/* className='right' */}
 
 
@@ -183,4 +47,4 @@ const Featured = () => {
     )
 }
 
-export default Featured
+export default Featured;
